299d11ec529e7674ae3e063dd91edcbe6aec8523
codybinu <binayak.bidyasagar99@gmail.com>
2023-04-16 23:30:48 +0530

    day 1 of making a portfolio

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import './Navbarstyles.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <Link to={'/'}>
+            <h1>Portfolio.</h1>
+        </Link>
+        <ul className='nav-menu'>
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link to={'/About'}>About</Link>
+            </li>
+            <li>
+                <Link to={'/Project'}>Project</Link>
+            </li>
+            <li>
+                <Link to={'/Contact'}>Contact</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
